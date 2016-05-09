4d5e492e1a0980435a3a227acb4ff1f472af9b0c
derekgilwa <dgliwa@thoughtworks.com>
2016-05-08 23:16:03 -0500

    Derek - finish first app with working app icon

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -13,12 +13,27 @@ class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
     
+    let questions: [String] = ["From what is cognac made?", "What is 7 + 7?", "What is the capital of Vermont"]
+    
+    let answers: [String] = ["Grapes", "14", "Montpelier"]
+    
+    var currentQuestionIndex = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+    
     @IBAction func showNextQuestion(sender: AnyObject) {
-        
+        currentQuestionIndex = (currentQuestionIndex + 1) % questions.count
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
     }
     
     @IBAction func showAnswer(sender: AnyObject) {
-        
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
     }
 }
 
