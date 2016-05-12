a4672f01af20d4cbe27f678b4eba0e9b32eaa476
derekgilwa <dgliwa@thoughtworks.com>
2016-05-12 00:14:51 -0400

    Animate changing of the questions.

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var currentQuestionLabel: UILabel!
+    @IBOutlet var nextQuestionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
     
     let questions: [String] = ["From what is cognac made?", "What is 7 + 7?", "What is the capital of Vermont"]
@@ -22,18 +23,18 @@ class ViewController: UIViewController {
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
-        questionLabel.alpha = 0
+        nextQuestionLabel.alpha = 0
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        questionLabel.text = questions[currentQuestionIndex]
+        currentQuestionLabel.text = questions[currentQuestionIndex]
     }
     
     @IBAction func showNextQuestion(sender: AnyObject) {
         currentQuestionIndex = (currentQuestionIndex + 1) % questions.count
         let question: String = questions[currentQuestionIndex]
-        questionLabel.text = question
+        nextQuestionLabel.text = question
         answerLabel.text = "???"
         animateLabelTransitions()
     }
@@ -45,7 +46,11 @@ class ViewController: UIViewController {
     
     func animateLabelTransitions() {
         UIView.animateWithDuration(0.5, animations: {
-            self.questionLabel.alpha = 1
+            self.currentQuestionLabel.alpha = 0
+            self.nextQuestionLabel.alpha = 1
+            }, completion: { _ in
+                swap(&self.currentQuestionLabel,
+                &self.nextQuestionLabel)
         })
     }
 }
